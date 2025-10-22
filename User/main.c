27aed8fb7221aadea5104aae0694a17fcd8a7fa9
xiaokai1213 <545923789@qq.com>
2025-10-22 22:11:35 +0800

    增加初始化状态机代码，加入上电事件

--- User/main.c
+++ User/main.c
@@ -30,7 +30,26 @@ int main() {
    bms_sm.current_event = event_power_on;  // 上电事件
    bms_sm.new_event = event_power_on;      // 上电事件
    while (1) {
-      /* code */
+      bms_sm_dispatch();  // 执行状态机调度函数
+      delay_ms(2);        // 延时2ms限制状态机执行频率
+   }
+}
+/**
+ * @brief   状态机调度函数
+ */
+void bms_sm_dispatch() {
+   switch (bms_sm.current_state) {  // 输入当前状态
+      case state_init:              // 初始化状态
+         init_execute();            // 初始化执行函数
+         break;
+      case state_self_test:  // 自检状态
+         break;
+      case state_runing:  // 运行状态
+         break;
+      case state_fault:  // 故障状态
+         break;
+      default:
+         break;
    }
 }
 
@@ -38,4 +57,13 @@ int main() {
  * @brief   状态机事件处理函数，状态机核心代码，根据当前事件决定状态转换
  * @param   event：要处理的事件
  */
-void bms_sm_handle_event(bms_event_t event) {}
+void bms_sm_handle_event(bms_event_t event) {
+   switch (event) {
+      case event_power_on:
+         init_execute();  // 初始化执行函数
+         break;
+
+      default:
+         break;
+   }
+}
--- User/main.h
+++ User/main.h
@@ -8,12 +8,11 @@
 
 // 状态枚举定义-列出bms从控所有可能状态
 typedef enum {
-   state_init = 0,    // 初始化：外设初始化；ltc6804初始化；等待接收主控关键数据
-   state_self_test,   // 自检状态：检查ltc6804关键数据
-   state_idle,        // 空闲状态
-   state_fault,       // 故障状态
-   state_working,     // 工作状态
-   state_receive_cmd  // 接收主控命令
+   state_init = 0,   // 初始化：外设初始化；ltc6804初始化；等待接收主控关键数据
+   state_self_test,  // 自检状态：检查ltc6804关键数据
+   state_fault,      // 故障状态
+   state_runing,     // 运行状态
+   state_idle        // 空闲状态
 } bms_state_t;
 
 // 事件枚举定义-定义触发状态转换的所有事件
@@ -33,5 +32,7 @@ typedef struct {
 extern bms_state_machine_t bms_sm;  // bms当前主状态枚举定义
 
 // 函数声明
+void bms_sm_dispatch();                       // 状态机调度函数
+void bms_sm_handle_event(bms_event_t event);  // 状态机事件处理函数，状态机核心代码，根据当前事件决定状态转换
 
 #endif  // _MAIN_H
