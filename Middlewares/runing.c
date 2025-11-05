82c5c01017b62bbbb3a6ec21aab36e3d875cf8de
xiaokai <545923789@qq.com>
2025-11-05 09:36:52 +0800

    紧接上次提交

--- Middlewares/runing.c
+++ Middlewares/runing.c
@@ -7,12 +7,5 @@
  * @brief   运行执行函数
  */
 void runing_execute(void) {
-   if (event_trigger == event_voltage_collect) {
-      ltc6804_Get_Voltage();  // 获取电池组电压，并存储到电池电压全局句柄
-   }
-
-   if (event_trigger == event_temp_collect) {
-      ltc6804_Get_temperature();  // 获取电池组极耳温度，并存储到辅助电压全局句柄
-   }
-   bms_sm_handle_event(event_enter_standby);  // 任务执行结束，进入待机状态
+      bms_sm_handle_event(event_enter_standby);  // 任务执行结束，进入待机状态
 }
